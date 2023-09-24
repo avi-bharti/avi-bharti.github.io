f3846062ff63817dc7ad4428ae55b734b097f8c0
avinash bharti <avinashbharti867@gmail.com>
2023-09-24 15:37:38 +0530

    updated skills

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+
+window.addEventListener('load', function(){
+  const startDate = new Date('04/01/2023');
+  const endDate = new Date();
+  let diffDays = parseInt((endDate - startDate) / (1000 * 60 * 60 * 24)); 
+  const year = (diffDays > 365) ? parseInt(diffDays/365) + ' Years' : ''; 
+  if(diffDays > 365){
+    diffDays = diffDays % 365;
+  }
+  const month = parseInt(diffDays / 30) + ' months';
+  document.querySelector('.time-in-job').innerHTML = `${year} ${month}`
+})
